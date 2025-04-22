2045d2954c0b647811baa68de97fb40163c3067c
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2025-04-22 15:06:40 +0100

    add Product model with schema definition

---
+++ src/models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		description: String,
+		price: {
+			type: Number,
+			required: true,
+		},
+		image: String,
+		collection: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Collection",
+		},
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
